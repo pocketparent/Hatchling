7066459a03875b40e4ffd67b71f015a6b18b84bd
pocketparent <dianamezzanotte@gmail.com>
2025-04-13 14:27:12 -0400

    Create .eslintrc.js

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: './tsconfig.json'
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    // Critical rules to prevent deployment errors
+    '@typescript-eslint/explicit-function-return-type': ['warn', {
+      allowExpressions: true,
+      allowTypedFunctionExpressions: true
+    }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { 
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_' 
+    }],
+    
+    // React-specific rules
+    'react/prop-types': 'off', // Since we're using TypeScript
+    'react/react-in-jsx-scope': 'off', // Not needed in React 17+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
